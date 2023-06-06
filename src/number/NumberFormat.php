a65cac5ec495561924d5a48f8ddcc013c9a69a9e
holdyouforever <416248856@qq.com>
2023-06-06 10:46:08 +0800

    commit

---
+++ src/number/NumberFormat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace numberFormat\number;
+
+class NumberFormat
+{
+    public static function desc()
+    {
+        var_dump( 'NumberFormat');
+    }
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+use numberFormat\number\NumberFormat;
+
+require 'vendor/autoload.php';
+
+NumberFormat::desc();
